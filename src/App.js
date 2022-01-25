c13c97d8867bb9afa007ec56b5bf34c64fed14ca
RolandYambao <rolandyambao@gmail.com>
2022-01-25 14:08:40 -0800

    Working on Gallery

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import Login from './components/Login';
 import Navbar from './components/Navbar';
 import Profile from './components/Profile';
 import Welcome from './components/Welcome';
+import Gallery from './components/Gallery';
 
 const PrivateRoute = ({ component: Component, ...rest}) => {
   let token = localStorage.getItem('jwtToken');
@@ -68,6 +69,7 @@ function App() {
             path="/login"
             render={(props) => <Login {...props} nowCurrentUser={nowCurrentUser} setIsAuthenticated={setIsAuthenticated} user={currentUser}/>}
           />
+          <PrivateRoute path="/gallery" component={Gallery} user={currentUser} handleLogout={handleLogout} />
           <PrivateRoute path="/profile" component={Profile} user={currentUser} handleLogout={handleLogout} />
           <Route exact path="/" component={Welcome} />
           <Route path="/about" component={About} />
---
+++ src/components/Gallery.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import './Gallery.css';
+
+class Gallery extends Component {
+    render() {
+        return (
+            <div>
+                <div class="main1">
+                    <ul>
+                        <li>
+                            <div class="slidetitle">
+                                <span>Eye</span>
+                            </div>
+                            <a href='#'>
+                                <img src="https://i.imgur.com/6DHX0zB.jpeg" />
+                            </a>
+                        </li>
+                        <li>
+                            <div class="slidetitle">
+                                <span>Eye</span>
+                            </div>
+                            <a href='#'>
+                                <img src="https://i.imgur.com/6DHX0zB.jpeg" />
+                            </a>
+                        </li>
+                        <li>
+                            <div class="slidetitle">
+                                <span>Eye</span>
+                            </div>
+                            <a href='#'>
+                                <img src="https://i.imgur.com/6DHX0zB.jpeg" />
+                            </a>
+                        </li>
+                        <li>
+                            <div class="slidetitle">
+                                <span>Eye</span>
+                            </div>
+                            <a href='#'>
+                                <img src="https://i.imgur.com/6DHX0zB.jpeg" />
+                            </a>
+                        </li>
+                        <li>
+                            <div class="slidetitle">
+                                <span>Eye</span>
+                            </div>
+                            <a href='#'>
+                                <img src="https://i.imgur.com/6DHX0zB.jpeg" />
+                            </a>
+                        </li>
+                        <li>
+                            <div class="slidetitle">
+                                <span>Eye</span>
+                            </div>
+                            <a href='#'>
+                                <img src="https://i.imgur.com/6DHX0zB.jpeg" />
+                            </a>
+                        </li>
+                        <li>
+                            <div class="slidetitle">
+                                <span>Eye</span>
+                            </div>
+                            <a href='#'>
+                                <img src="https://i.imgur.com/6DHX0zB.jpeg" />
+                            </a>
+                        </li>
+                        <li>
+                            <div class="slidetitle">
+                                <span>Eye</span>
+                            </div>
+                            <a href='#'>
+                                <img src="https://i.imgur.com/6DHX0zB.jpeg" />
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Gallery;
