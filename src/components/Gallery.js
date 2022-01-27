fc59f09c00186684edb9387b9863c72732f00ea6
RolandYambao <rolandyambao@gmail.com>
2022-01-27 09:29:51 -0800

    Updated

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -76,52 +76,52 @@ class Gallery extends Component {
                         <h1 id="galleryTitle">Muses Exhibit</h1>
                         <br />
                         <li id="no-js-slider-1" class="slide">
-                            <img src="https://i.imgur.com/6DHX0zB.jpeg" class="galleryImage" />
+                            <img src="https://i.imgur.com/6DHX0zB.jpeg" class="galleryImage" alt="Placeholder" />
                             <a class="prev" href="#no-js-slider-10">prev</a>
                             <a class="next" href="#no-js-slider-2">next</a>
                         </li>
                         <li id="no-js-slider-2" class="slide">
-                            <img src="https://i.imgur.com/G5btH1u.jpeg" class="galleryImage" />
+                            <img src="https://i.imgur.com/G5btH1u.jpeg" class="galleryImage" alt="Placeholder" />
                             <a class="prev" href="#no-js-slider-1">prev</a>
                             <a class="next" href="#no-js-slider-3">next</a>
                         </li>
                         <li id="no-js-slider-3" class="slide">
-                            <img src="https://i.imgur.com/Jssc0Oc.jpeg" class="galleryImage" />
+                            <img src="https://i.imgur.com/Jssc0Oc.jpeg" class="galleryImage" alt="Placeholder" />
                             <a class="prev" href="#no-js-slider-2">prev</a>
                             <a class="next" href="#no-js-slider-4">next</a>
                         </li>
                         <li id="no-js-slider-4" class="slide">
-                            <img src="https://i.imgur.com/p49Y6Yp.jpeg" class="galleryImage" />
+                            <img src="https://i.imgur.com/p49Y6Yp.jpeg" class="galleryImage" alt="Placeholder" />
                             <a class="prev" href="#no-js-slider-3">prev</a>
                             <a class="next" href="#no-js-slider-5">next</a>
                         </li>
                         <li id="no-js-slider-5" class="slide">
-                            <img src="https://i.imgur.com/qCPxIrx.jpeg" class="galleryImage" />
+                            <img src="https://i.imgur.com/qCPxIrx.jpeg" class="galleryImage" alt="Placeholder" />
                             <a class="prev" href="#no-js-slider-4">prev</a>
                             <a class="next" href="#no-js-slider-6">next</a>
                         </li>
                         <li id="no-js-slider-6" class="slide">
-                            <img src="https://i.imgur.com/gxLmt4S.jpeg" class="galleryImage" />
+                            <img src="https://i.imgur.com/gxLmt4S.jpeg" class="galleryImage" alt="Placeholder" />
                             <a class="prev" href="#no-js-slider-5">prev</a>
                             <a class="next" href="#no-js-slider-7">next</a>
                         </li>
                         <li id="no-js-slider-7" class="slide">
-                            <img src="https://i.imgur.com/O3DMsIb.jpeg" class="galleryImage" />
+                            <img src="https://i.imgur.com/O3DMsIb.jpeg" class="galleryImage" alt="Placeholder" />
                             <a class="prev" href="#no-js-slider-6">prev</a>
                             <a class="next" href="#no-js-slider-8">next</a>
                         </li>
                         <li id="no-js-slider-8" class="slide">
-                            <img src="https://i.imgur.com/HhzHAgy.jpeg" class="galleryImage" />
+                            <img src="https://i.imgur.com/HhzHAgy.jpeg" class="galleryImage" alt="Placeholder" />
                             <a class="prev" href="#no-js-slider-7">prev</a>
                             <a class="next" href="#no-js-slider-9">next</a>
                         </li>
                         <li id="no-js-slider-9" class="slide">
-                            <img src="https://i.imgur.com/RJPDuSY.jpeg" class="galleryImage" />
+                            <img src="https://i.imgur.com/RJPDuSY.jpeg" class="galleryImage" alt="Placeholder" />
                             <a class="prev" href="#no-js-slider-8">prev</a>
                             <a class="next" href="#no-js-slider-10">next</a>
                         </li>
                         <li id="no-js-slider-10" class="slide">
-                            <img src="https://i.imgur.com/Kf1JGmf.jpeg" class="galleryImage" />
+                            <img src="https://i.imgur.com/Kf1JGmf.jpeg" class="galleryImage" alt="Placeholder" />
                             <a class="prev" href="#no-js-slider-9">prev</a>
                             <a class="next" href="#no-js-slider-1">next</a>
                         </li>
