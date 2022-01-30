8e8230d7a7af8094295ca126cb775d05d63d9ec3
RolandYambao <rolandyambao@gmail.com>
2022-01-29 16:38:09 -0800

    Fixed Aesthetic

--- src/components/DeletePortfolio.js
+++ src/components/DeletePortfolio.js
@@ -51,7 +51,7 @@ class EditPortfolio extends Component {
             <div>
                 <div className="box">
                     <figure className="avatar">
-                        <img src="https://i.imgur.com/iLl2PQ2.png" id="portfolioLogo" alt="Minus Sign" />
+                        <img src="https://i.imgur.com/smPpriY.png" id="portfolioLogo" alt="Minus Sign" />
                     </figure>
                     <form id="portfolioInputSection" onSubmit={this.handleSubmit.bind(this)}>
                         <div className="field">
